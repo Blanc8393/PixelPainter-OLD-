64f3acc245eecb971c7eb240b11c513858a34d42
Blanc8393 <cste5546@gmail.com>
2023-02-15 23:00:36 -0700

    Check if files exist Fixes #2

--- ImageToPainting.py
+++ ImageToPainting.py
@@ -3,31 +3,52 @@ from PIL import Image
 
 directory = "Input"
 
-def multipleImages():
-    for file in os.listdir(directory):
-        userInput = input(f"Enter block dimensions for {file} (w h) or \"s\" to skip:\n")
-        dimensions = userInput.split()
-        if(dimensions[0] != "s"):
-            width = int(dimensions[0]) * 16
-            height = int(dimensions[1]) * 16
-            image = Image.open("Input/" + file)
-            image.save("Output/MCPaint_" + file + ".png")
-            image = image.resize((width, height), Image.Resampling.BILINEAR)
-            image = image.resize((image.width * 8, image.height * 8), Image.Resampling.NEAREST)
-            image.save("Output/MCPaint_" + file + ".png")
+def FileExists(filename):
+    try:
+        Image.open(filename)
+        return True
+    except:
+        print("Invalid file name, please enter a valid file.\n")
+        return False
 
-def singleImage():
-    print()
+def GetBlockDimensions(filename):
+    userInput = input(f"Enter block dimensions for {filename} (w h) or \"s\" to skip:\n")
+    if(userInput[0] == 's'):
+        return 's'
+    while(len(userInput) != 3):
+        print(len(userInput))
+        userInput = input("Only 2 arguments allowed. Enter width and height (w h)\n")
+    return userInput.split()
+
+def ConvertToPainting(dimensions, input):
+    width = int(dimensions[0]) * 16
+    height = int(dimensions[1]) * 16
+    image = Image.open("Input/" + input)
+    image = image.resize((width, height), Image.Resampling.BILINEAR)
+    image = image.resize((width * 8, height * 8), Image.Resampling.NEAREST)
+    noExt = input.partition(".")[0]
+    image.save("Output/PXLPNT_" + noExt + ".png")
+
+def SingleImage(filename):
+    if(FileExists(filename)):
+        dimensions = GetBlockDimensions()
+        image = Image.open(filename)
+        ConvertToPainting(dimensions, image)
+
+def MultipleImages():
+    for file in os.listdir(directory):
+        dimensions = GetBlockDimensions(file)
+        ConvertToPainting(dimensions, file)
 
 def main():
     modePrompt = input("Single File (s) or Multiple Files (m)?\n")
-    match modePrompt:
-        case 's':
-            image = input("Image name in Input folder:\n")
-        case 'm':
-            multipleImages()
-        case _:
-            print("Please select 's' or 'm'")
+    if(modePrompt == 's'):
+        filename = input("Image name in Input folder:\n")
+        SingleImage(filename)
+    elif(modePrompt == 'm'):
+        MultipleImages()
+    else:
+        print("Please select 's' or 'm'\n")
         
 
 if(__name__ == "__main__"):
