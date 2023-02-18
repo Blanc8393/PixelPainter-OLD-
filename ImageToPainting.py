90611b0f619a9d6839bc0c4e58fccedbc32b02f9
Blanc8393 <cste5546@gmail.com>
2023-02-18 16:22:13 -0700

    Create a "Choose Folder Prompt" Fixes #11

--- ImageToPainting.py
+++ ImageToPainting.py
@@ -1,27 +1,91 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
+
 from PIL import Image
 
-def CreateFolders():
-    try:
-        os.mkdir("Input")
-    except FileExistsError:
-        pass
+INPUT_FOLDER = ""
+INPUT_IMAGE = ""
+OUTPUT_FOLDER = ""
+NAME_EACH = ""
+IS_FOLDER = ""
 
-    try:
-        os.mkdir("Output")
-    except FileExistsError:
-        pass
+#Setting to folder or single mode
+def SetIsFolder():
 
-def ImageExists(filename):
+    global IS_FOLDER
+    selection = input("Folder of images, single image, or cancel?\nf/s/c: ")
+    
+    while(True):
+        if(selection == 'f'):
+            IS_FOLDER = True
+            return
+
+        elif(selection == 's'):
+            IS_FOLDER = False
+            return
+
+        elif(selection == 'c'):
+            print("Cancelling...")
+            exit()
+
+        else:
+            print("Please choose 'f', 's', or 'c'")
+
+#Setting naming mode
+def SetNameEach():
+
+    global NAME_EACH
+    selection = input("Would you like to rename your image(s)?\ny/n: ")
+    
+    while(True):
+        if(selection == 'y'):
+            NAME_EACH = True
+            return
+        elif(selection == 'n'):
+            NAME_EACH = False
+            return
+        else:
+            print("Please enter 'y' or 'n'")
+
+def ValidateImage():
     try:
-        img = Image.open("Input/" + filename)
+        Image.open(INPUT_IMAGE)
         return True
     except IOError:
-        print(f"\n{filename} is an invalid image.\n")
+        print(f"\n{INPUT_IMAGE} is not a valid image.\n")
         return False
 
+#Set input FOLDER or IMAGE (Image is validated)
+def SetInput():
+    global IS_FOLDER
+    global INPUT_FOLDER
+    global INPUT_IMAGE
+
+    root = tk.Tk()
+    root.withdraw()
+
+    if(IS_FOLDER):
+        print("Choose a folder")
+        INPUT_FOLDER = filedialog.askdirectory()
+        return
+    else:
+        print("Choose a file")
+        while(True):
+            INPUT_IMAGE = filedialog.askopenfile().name
+            if(ValidateImage() == True):
+                return
+
+#Set Output Folder  
+def SetOutput():
+    global OUTPUT_FOLDER
+    print("Choose an output folder")
+    OUTPUT_FOLDER = filedialog.askdirectory()
+    return
+
+#Get desired dimensions (in blocks) of final painting
 def GetBlockDimensions(filename):
-    userInput = input(f"Enter block dimensions for {filename} (w h) or \"s\" to skip: ")
+    userInput = input(f"Enter block dimensions for {filename} (w h) or \"s\" to skip: ") #filename is ugly
     if(userInput[0] == 's'):
         return 's'
     while(len(userInput) != 3):
@@ -29,37 +93,45 @@ def GetBlockDimensions(filename):
         userInput = input("Only 2 arguments allowed. Enter width and height (w h)\n")
     return userInput.split()
 
-def ConvertToPainting(dimensions, input):
+#dimensions is a tuple, input should be an image
+#PROBLEM IS HERE
+def ImageToPainting(dimensions, image):
     width = int(dimensions[0]) * 16
     height = int(dimensions[1]) * 16
-    image = Image.open("Input/" + input)
-    image = image.resize((width, height), Image.Resampling.BILINEAR)
-    image = image.resize((width * 8, height * 8), Image.Resampling.NEAREST)
-    noExt = input.partition(".")[0]
-    image.save("Output/PXLPNT_" + noExt + ".png")
+    painting = image.resize((width, height), Image.Resampling.BILINEAR)
+    painting = painting.resize((width * 8, height * 8), Image.Resampling.NEAREST)
 
-def SingleImage(filename):
-    if(ImageExists(filename)):
-        dimensions = GetBlockDimensions()
-        image = Image.open(filename)
-        ConvertToPainting(dimensions, image)
+    if(NAME_EACH == True):
+        newName = input("Enter new name for \"" + image.filename + "\", excluding file extensions: ")
+        image.save(OUTPUT_FOLDER + "PXLPNT_" + newName + ".png")
+        return
+    noExt = image.filename.partition(".")[0]
+    image.save(OUTPUT_FOLDER + "PXLPNT_" + noExt + ".png")
 
-def MultipleImages():
-    for file in os.listdir("Input"):
-        if(ImageExists(file)):
-            dimensions = GetBlockDimensions(file)
-            ConvertToPainting(dimensions, file)
+def ConvertAndSave():
+    global IS_FOLDER
+    global INPUT_FOLDER
+    global OUTPUT_FOLDER
+    global INPUT_IMAGE
+
+    if(IS_FOLDER):
+        for file in os.listdir(INPUT_FOLDER):
+            dimensions = GetBlockDimensions(file.filename)
+            image = Image.open(file)
+            if(dimensions != 's'):
+                ImageToPainting(dimensions, image)
+    else:
+        dimensions = GetBlockDimensions(INPUT_IMAGE)
+        image = Image.open(INPUT_IMAGE)
+        if(dimensions != 's'):
+                ImageToPainting(dimensions, image)
 
 def main():
-    CreateFolders()
-    modePrompt = input("Single File (s) or Multiple Files (m)? ")
-    if(modePrompt == 's'):
-        filename = input("Image name in Input folder:\n")
-        SingleImage(filename)
-    elif(modePrompt == 'm'):
-        MultipleImages()
-    else:
-        print("Please select 's' or 'm'\n")
+    SetIsFolder()
+    SetOutput()
+    SetInput()
+    SetNameEach()
+    ConvertAndSave()
         
 
 if(__name__ == "__main__"):
